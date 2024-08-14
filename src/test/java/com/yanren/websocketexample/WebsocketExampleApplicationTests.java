41d94a0712ce21476f305515bbe547ac42cd08d9
yanren <easy10441@gmail.com>
2024-08-14 11:28:45 +0800

    spring init

---
+++ src/test/java/com/yanren/websocketexample/WebsocketExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yanren.websocketexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsocketExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
